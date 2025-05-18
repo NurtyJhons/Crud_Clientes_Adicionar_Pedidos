1dbc860b064fbdd58a8ba2573cba611ac4dc756b
João Vitor <joaovitortargueta@gmail.com>
2025-05-18 15:59:02 -0300

    Primeiro commit do projeto com carrinho de compras

--- crud_api/crud_api/urls.py
+++ crud_api/crud_api/urls.py
@@ -18,11 +18,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API de Clientes",
+      default_version='v1',
+      description="Documentação da API do painel de clientes",
+      contact=openapi.Contact(email="seu@email.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('clientes/', include('core.urls')),  # Para as páginas HTML dos clientes finais
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
